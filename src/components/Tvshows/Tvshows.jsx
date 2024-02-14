0269ab79a9a5b8db3ea3132d4cc39e4918eeb376
Sourabh <137615880+Sourabhpdnker@users.noreply.github.com>
2024-02-15 00:09:15 +0530

    second

--- src/components/Tvshows/Tvshows.jsx
+++ src/components/Tvshows/Tvshows.jsx
@@ -1,28 +1,20 @@
 function Tvshows() {
   return (
     <>
-      <div class="w-screen h-screen bg-center bg-cover bg-black border-t-[8px] border-teal-500">
-        <h1 class="sm:text-[25px] m-6 text-[10px] font-medium text-white">
-          TV Show.
-        </h1>
-        <div className="flex">
-          <div>
-            <div class="flex-1 text-white mx-20">
-              <h1 class="sm:text-[45px] text-[20px] font-bold">
-                Unlimited TV shows and more.
-              </h1>
-              <h1 class="sm:text-[18px] py-2 text-[7px] font-medium">
-                Watch anywhere.cancel anytime
-              </h1>
-              <p class="sm:text-[15px] text-[6px] font-normal">
-                Ready to Watch? Enter your email to create or restart your
-                membership.
-              </p>
-            </div>
+      <div class="w-screen h-screen bg-center bg-cover bg-black border-t-[8px] border-teal-500 sm:py-[150px] py-[50px] text-start flex">
+        <div>
+          <div class="flex-1 text-white mx-20">
+            <h1 class="sm:text-[58px] text-[30px] font-bold">
+              Enjoy on your Tv.
+            </h1>
+            <h1 class="sm:text-[23px] py-2 text-[11px] font-medium">
+              Watch on smart Tvs, PlayStation, Xbox, charomecast,Apple
+              TV,Blue-ray players and more
+            </h1>
           </div>
-          <div>
-            <div class="mx-40 w-64 h-64 bg-[url('./assets/images/mov1.png')] object-contain  bg-center bg-cover flex-1 justify-center"></div>
-          </div>
+        </div>
+        <div>
+          <div class="mx-40 w-64 h-64 bg-[url('./assets/images/mov1.png')] object-contain  bg-center bg-cover flex-1 justify-center"></div>
         </div>
       </div>
     </>
